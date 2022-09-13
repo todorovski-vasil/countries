9e2ca582f668f18ea03e86690595ea860cc20182
Vasil Todorovski <vasil.todorovski@intertec.io>
2022-09-13 23:33:25 +0200

    Added tailwindcss, react-icons, classnames
    Created the basic theme with a dark mode switcher
    
    Imported the Nunito font

---
+++ components/CountryTile/CountryTile.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import classNames from "classnames";
+
+const CountryTile = () => {
+  return <div className={classNames("p-10 ")}>Country</div>;
+};
+
+export default CountryTile;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ["nunito", "sans-serif"],
+      },
+      colors: {
+        "dark-blue": "hsl(209, 23%, 22%)", // Dark Blue (Dark Mode Elements)
+        "midnight-blue": "hsl(207, 26%, 17%)", // Very Dark Blue (Dark Mode Background)
+        "very-dark-blue": "hsl(200, 15%, 8%)", // Very Dark Blue (Light Mode Text)
+        "dark-gray-input": "hsl(0, 0%, 52%)", // Dark Gray (Light Mode Input)
+        "very-light-gray": "hsl(0, 0%, 98%)", // Very Light Gray (Light Mode Background)
+        white: "hsl(0, 0%, 100%)", // White (Dark Mode Text & Light Mode Elements)
+      },
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+};
